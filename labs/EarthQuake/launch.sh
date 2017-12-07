e79a7fd1c1236fe0fc0f72c7d4e3f44933c3f53a
Leonid Kuligin <kuligin@kuligin-macbookair.roam.corp.google.com>
2017-12-07 11:50:56 +0100

    lab adjustment

--- labs/EarthQuake/launch.sh
+++ labs/EarthQuake/launch.sh
@@ -1,5 +1,21 @@
-gcloud compute --project "leonid-on-boarding" instances create "earthquake-1" \
---zone "europe-west1-b" --machine-type "n1-standard-1" --subnet "default" --no-restart-on-failure --maintenance-policy "TERMINATE" \
---service-account "157762861736-compute@developer.gserviceaccount.com" \
---scopes "https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
---image "ubuntu-1704-zesty-v20171011" --image-project "ubuntu-os-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "earthquake-1"
+NAME="earthquake-1"
+
+GCLOUD="/Users/kuligin/Documents/tools/google-cloud-sdk/bin/gcloud"
+RAWSTATUS="$($GCLOUD compute instances list --filter="name:$NAME")"
+STATUS="$(echo $RAWSTATUS | awk '{print $NF}')"
+
+if [ "$STATUS" = "" ]
+then
+	echo "creating the VM"
+	$GCLOUD compute --project "leonid-on-boarding" instances create "$NAME" \
+	--zone "europe-west1-b" --machine-type "n1-standard-1" --subnet "default" \
+	--no-restart-on-failure --maintenance-policy "TERMINATE" \
+	--service-account "157762861736-compute@developer.gserviceaccount.com" \
+	--scopes "https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+	--image "ubuntu-1704-zesty-v20171011" --image-project "ubuntu-os-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "earthquake-1"
+elif [ "$STATUS" == "TERMINATED" ]
+then
+	echo "TERMINATED"
+else 
+	echo "status is $STATUS"
+fi
--- labs/earthquake/run.sh
+++ labs/earthquake/run.sh
@@ -6,11 +6,13 @@ sudo apt-get --fix-missing --yes install python3-mpltoolkits.basemap
 sudo apt-get build-dep --yes python3-matplotlib
 
 sudo /usr/bin/easy_install virtualenv
+cd ..
 mkdir py3
 virtualenv --system-site-packages -p python3 py3
-echo 'alias py3="source /home/lkulighin/py3/bin/activate"' >>~/.bash_profile
+echo 'alias py3="source /home/kuligin/py3/bin/activate"' >>~/.bash_profile
 source ~/.bash_profile
 
 py3
-python calculate.py3
+cd earthquake
+python calculate.py
 gsutil cp earthquakes.png gs://test-1113
